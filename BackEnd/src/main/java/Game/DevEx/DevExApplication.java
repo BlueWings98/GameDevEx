ff5f7eac176675e0dad3c3c7bcb5ad3bdf710d87
BlueWings98 <sebastiannovoa1998@hotmail.com>
2024-06-19 10:00:30 -0500

    First commit

---
+++ BackEnd/src/main/java/Game/DevEx/DevExApplication.java
@@ -0,0 +1,13 @@
+package Game.DevEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevExApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/Game/DevEx/DevExApplicationTests.java
@@ -0,0 +1,13 @@
+package Game.DevEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
