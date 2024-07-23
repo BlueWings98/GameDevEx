0697be2ddbe1a1a6e8408b0c0956e3447778e7de
BlueWings98 <sebastiannovoa1998@hotmail.com>
2024-07-23 10:12:28 -0500

    Funciona animaciones y campo defintivo

--- FrontEnd/front-end/src/pages/Survey.js
+++ FrontEnd/front-end/src/pages/Survey.js
@@ -2,27 +2,78 @@ import React from "react";
 import Phaser from "phaser";
 import GameComponent from "../components/GameComponent";
 
-const spritesDir = '../assets/sprites/morpeko/';
+const spritesDir = '../assets/sprites/morpeko/SpriteSheets/';
 const backgroundDir = '../assets/background/';
+const width= window.innerWidth;
+const height= window.innerHeight;
+const imageHeight = 1390;
+const imageWidth = 1417;
 
 class Survey extends Phaser.Scene {
     preload() {
-      
-        this.load.image('barn', `${backgroundDir}barn.png`);
-        this.load.image('A1base', `${spritesDir}A1/A1-base.png`);
-        this.load.image('A2base', `${spritesDir}A2/A2-base.png`);
+        this.load.image('barn', `${backgroundDir}CampoCasilla.png`);
+        this.load.spritesheet("Alegre", `${spritesDir}A.png`, { frameWidth: imageWidth, frameHeight: imageHeight });
+        this.load.spritesheet("Triste", `${spritesDir}T.png`, { frameWidth: imageWidth, frameHeight: imageHeight });
+        this.load.spritesheet("Enojado", `${spritesDir}E.png`, { frameWidth: imageWidth, frameHeight: imageHeight });
+        this.load.spritesheet("Neutral", `${spritesDir}N.png`, { frameWidth: imageWidth, frameHeight: imageHeight });
       }
     
 
     create() {
-        const { width, height } = this.scale;
         this.barn = this.add.image(width / 2, height / 2, 'barn');
         this.barn.displayWidth = width;
         this.barn.displayHeight = height;
-        this.A1base = this.add.image(width/2, height-180, 'A1base').setScale(0.2);
+        this.addSprites();
+        this.createAnimations();
     }
     update(){
         //this.A1base.x += 1;
+    }
+    createAnimations(){
+        const fps = 0.5;
+        let test = this.anims.generateFrameNumbers("Alegre");
+        console.log(test);
+        this.anims.create({
+            key: "AnimAlegre",
+            frames: this.anims.generateFrameNumbers("Alegre"),
+            frameRate: fps,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "AnimTriste",
+            frames: this.anims.generateFrameNumbers("Triste"),
+            frameRate: fps,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "AnimEnojado",
+            frames: this.anims.generateFrameNumbers("Enojado"),
+            frameRate: fps,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "AnimNeutral",
+            frames: this.anims.generateFrameNumbers("Neutral"),
+            frameRate: fps,
+            repeat: -1
+        });
+
+        this.Alegre.play("AnimAlegre");
+        this.Triste.play("AnimTriste");
+        this.Enojado.play("AnimEnojado");
+        this.Neutral.play("AnimNeutral");
+    }
+    addSprites(){
+        const floorHeight = 500;
+
+        this.Alegre = this.add.sprite(width-600, height-floorHeight-50, "Alegre");
+        this.Alegre.setScale(0.3);
+        this.Triste = this.add.sprite(width-1000, height-floorHeight-50, "Triste");
+        this.Triste.setScale(0.3);
+        this.Enojado = this.add.sprite(width-1700, height-floorHeight-50, "Enojado");
+        this.Enojado.setScale(0.3);
+        this.Neutral = this.add.sprite(width-2100, height-floorHeight-50, "Neutral");
+        this.Neutral.setScale(0.3);
     }
 }
     const SurveyPage = () => {
